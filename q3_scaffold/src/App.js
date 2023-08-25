da609698068a2ba4d698666ebd0930b51210e388
KRISHNA MAHATO <mkrishna98212@gmail.com>
2023-08-25 09:15:02 +0530

    Debug the react app to provide store access to the components.

---
+++ q3_scaffold/src/App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+import { Actions } from "./components/Actions";
+import { Timer } from "./components/Timer";
+//  add redux imports here
+import { Provider } from "react-redux";
+import { store } from "./store.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Simple Timer</h1>
+      <Provider store={store}>
+        <Timer />
+        <Actions/>
+      </Provider>
+    </div>
+  );
+}
