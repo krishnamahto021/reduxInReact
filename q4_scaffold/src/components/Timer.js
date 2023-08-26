5587af0c5a1de9ae6ae899928dc0342e27f9dd30
KRISHNA MAHATO <mkrishna98212@gmail.com>
2023-08-26 09:07:48 +0530

    Dispatch appropriate actions to make the Timer app function correctly.

---
+++ q4_scaffold/src/components/Timer.js
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { incrementTimer } from "../redux/actions/timerActions";
+
+export const Timer = () => {
+  const { isRunning, elapsedTime } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    let interval;
+    if (isRunning) {
+      interval = setInterval(() => {
+        // disptach incrementTimer action here
+        dispatch(incrementTimer());        
+      }, 1000);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isRunning,dispatch]);
+
+  return <b>{elapsedTime}</b>;
+};
